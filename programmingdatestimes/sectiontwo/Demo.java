5f57ea47ca54c9f372d011070ee4240c51f020df
Arthur Brombacher <abrombacher@suvoda.com>
2022-10-10 16:44:45 -0400

    Started Programming with Dates and Times in Java

---
+++ programmingdatestimes/sectiontwo/Demo.java
@@ -0,0 +1,30 @@
+package programmingdatestimes.sectiontwo;
+
+/**
+ * Demo Application TODO List, but with a difference
+ * Starting point is a work schedule
+ *      - Including fixed events, like meetings
+ * User guesses the duration of each task
+ * System allocates tasks to work periods
+ *      - Might need to split tasks between periods
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello Pluralsight!");
+
+        // create a schedule calendar
+
+        // add some tasks to the schedule calendar
+
+        // add some work periods to the schedule calendar
+
+        // add an event to the schedule calendar
+
+        // create a working schedule
+
+        // and print it out
+
+
+
+    }
+}
